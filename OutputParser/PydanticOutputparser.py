c36de59d398a36ac06a5461f52602604a0f4f90e
Anurag Selote <anuragselote12@gmail.com>
2025-05-20 12:47:03 +0530

    pydanticOutputparser file is added

---
+++ OutputParser/PydanticOutputparser.py
@@ -0,0 +1,43 @@
+from altair import Feature
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from regex import template
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+class Person(BaseModel):
+    
+    name: str = Field(description="Name of the person")
+    age: int = Field(gt=18, description="Age of the person")
+    city: str = Field(description='Name of the city the person belongs to')
+    
+parser = PydanticOutputParser(pydantic_object= Person)
+
+template = PromptTemplate(
+    template= "Generate the name , age and city of a fictional {place} person \n {format_instruction}",
+    input_variables=['place'],
+    partial_variables=[{'format_instruction':parser.get_format_instructions()}]
+)
+
+# Chain structure
+chain  = template | model | parser
+final_result = chain.invoke({'place':'shrilanka'})
+# prompt = template.invoke(
+#     {'place':'indian'}
+# )
+# result = model.invoke(prompt)
+
+# final_result = parser.parse(result.conten)
+
+print(final_result) 
